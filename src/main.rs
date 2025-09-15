5ca886dc1619f8bc45848849e25b49907f0f903c
Matt Cuffaro <cuffaro.matt@gmail.com>
2025-09-15 08:58:05 -0400

    ENH: Hello, world!

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+#![allow(unused_must_use)]
+#![allow(unused_mut)]
+#![allow(unused_variables)]
+#![allow(deprecated)]
+
+fn main() {
+    println!("Hello, world!");
+}
